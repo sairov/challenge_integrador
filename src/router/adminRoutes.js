5f647e76f8ef2e0636408bdd24be84deea303f82
SairovARG <pmrovira92@gmail.com>
2023-07-10 00:47:47 -0300

    [FEAT] migrating to global route middleware at login

--- src/router/adminRoutes.js
+++ src/router/adminRoutes.js
@@ -5,12 +5,14 @@ const { isLogged } = require('../middlewares/login');
 
 const controllers = require('../controllers/adminControllers');
 
-router.get('/', isLogged, controllers.adminView);
-router.get('/create', isLogged, controllers.createView);
-router.post('/create', isLogged, uploadFiles.array('images', 2), controllers.createItem);
-router.post('/create/bulk', isLogged, controllers.bulkCreate);
-router.get('/edit/:id', isLogged, controllers.editView);
-router.put('/edit/:id', isLogged, controllers.editItem);
-router.delete('/delete/:id', isLogged, controllers.deleteItem);
+router.use(isLogged);
+
+router.get('/', controllers.adminView);
+router.get('/create', controllers.createView);
+router.post('/create', uploadFiles.array('images', 2), controllers.createItem);
+router.post('/create/bulk', controllers.bulkCreate);
+router.get('/edit/:id', controllers.editView);
+router.put('/edit/:id', controllers.editItem);
+router.delete('/delete/:id', controllers.deleteItem);
 
 module.exports = router;
