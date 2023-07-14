8d60d5f0ddc5226d730a1206ca312dbb19bbaae2
Pablo Martín Rovira <43736065+sairov@users.noreply.github.com>
2023-07-13 21:08:44 -0300

    [FIX] views paths (#16)
    * [FIX] views paths
    
    * [FIX] statics
    
    * [FIX] migrating to cookie-session

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const methodOverride = require('method-override');
 const { initSession } = require('./src/utils/sessions');
 require('dotenv').config();
@@ -16,12 +17,12 @@ const PORT = process.env.PORT || 3000;
 
 /* Define carpeta de archivos estáticos */
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 /* Configuración del Template Engine - EJS */
 
 app.set('view engine', 'ejs');
-app.set('views', './src/views');
+app.set('views', path.resolve(__dirname, "./src/views"));
 
 
 /* Creamos la session de usuario */
--- src/utils/sessions.js
+++ src/utils/sessions.js
@@ -1,11 +1,9 @@
-const session = require('express-session');
+const session = require('cookie-session');
 require('dotenv').config();
 
 function initSession() {
   return session({
-    secret: process.env.SESSION_NAME,
-    resave: false,
-    saveUninitialized: true,
+    secret: process.env.SESSION_NAME
   });
 };
 
